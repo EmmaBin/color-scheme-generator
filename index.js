bd516d5ae2521a8134a46362cd355c822cea8555
emmacodes <binma.emma@gmail.com>
2022-11-30 15:25:57 -0800

    initiate fetch

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const submitBtn= document.getElementById('submit-btn')
+const schemeForm = document.getElementById('scheme-form')
+const seed = document.getElementById('seed')
+const mode = document.getElementById('mode')
+
+schemeForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const seedValue = seed.value.slice(1)
+    const modeValue = mode.value
+    
+
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedValue}&mode=${modeValue}&count=5`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+})
 
